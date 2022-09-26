666471238478b69988f1a0ee04a4cdd6b65a300d
avgfx <avgfx15@gmail.com>
2022-09-26 09:10:21 +0530

    Set up New Angular App and create admin Module and some Component like header, footer, home

---
+++ frontend/src/app/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SellerSignupComponent } from './seller-signup/seller-signup.component';
+
+
+
+@NgModule({
+  declarations: [
+    SellerSignupComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
